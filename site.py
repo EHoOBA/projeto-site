abc41c32792578761f9eef6545920eb1d28a5d35
Matheus Vittorel Oba <matheusvittoreloba@hotmail.com>
2021-11-17 18:00:26 +0000

    site 1.1

---
+++ site.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+funcionarios = [
+    {'nome': 'Matheus o OBA','salario': 'R$ 5000'},
+    {'nome': 'Moises o CORREA','salario': 'R$ 22000'},
+    {'nome': 'Jonathan o GARCIA','salario': 'R$ 12000'},
+    {'nome': 'Lucas o RODRIGO','salario': 'R$ 569'},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', lista=funcionarios)
+
+@app.route('/adicionar')
+def adicionar():
+    return render_template('adicionar.html')
+
+@app.route('/save', methods=['POST'])  # <form action="/save" method="POST">
+def save():
+    nome = request.form['nome']
+    salario = request.form['salario']
+    funcionario = {'nome': nome, 'salario': 'R$ ' + salario}
+    funcionarios.append(funcionario)
+
+    return redirect('https://5000-chocolate-crocodile-1x66zhrm.ws-us18.gitpod.io')
+
+app.run(debug=True)
